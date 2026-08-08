35bf4f526e4114581052053608678853ff58607d
leewebstechnologies.ali <leewebstechnologies.aliuajibade317@gmail.com>
2026-08-07 23:50:18 -0700

    add validation message

--- resources/views/backend/amenity/add_amenity.blade.php
+++ resources/views/backend/amenity/add_amenity.blade.php
@@ -7,27 +7,56 @@
           <div class="col-md-8 col-xl-8 middle-wrapper">
             <div class="row">
                 <div class="card">
-              <div class="card-body">
-                <h6 class="card-title">Add Amenity</h6>
-                <form method="POST" action="{{ route('store.amenity') }}" class="forms-sample">
-                    @csrf
-                    <div class="mb-3">
-                        <label for="exampleInputName1" class="form-label">Amenity Name</label>
-                        <input type="text" name="amenities_name" class="form-control  @error('amenities_name') is-invalid @enderror">
-                        @error('amenities_name')
-                        <span class="text-danger">{{ $message }}</span>
-                        @enderror
+                    <div class="card-body">
+                        <h6 class="card-title">Add Amenity</h6>
+                        <form id="myForm" method="POST" action="{{ route('store.amenity') }}" class="forms-sample">
+                            @csrf
+                            <div class="form-group mb-3">
+                                <label for="exampleInputName1" class="form-label">Amenity Name</label>
+                                <input type="text" name="amenities_name" class="form-control">
+                            </div>
+                            <button type="submit" class="btn btn-primary me-2">Save Changes</button>
+                        </form>
                     </div>
-                    <button type="submit" class="btn btn-primary me-2">Save Changes</button>
-                </form>
-              </div>
-            </div>
+                </div>
 
             </div>
-          </div>
+        </div>
           <!-- middle wrapper end -->
           <!-- right wrapper start -->
-        </div>
-	</div>
+    </div>
+</div>
+
+    <script type="text/javascript">
+        $(document).ready(function (){
+            $('#myForm').validate({
+                rules: {
+                    amenities_name: {
+                        required : true,
+                    },
+
+                },
+                messages :{
+                    amenities_name: {
+                        required : 'Please Enter Amenities Name',
+                    },
+
+
+                },
+                errorElement : 'span',
+                errorPlacement: function (error,element) {
+                    error.addClass('invalid-feedback');
+                    element.closest('.form-group').append(error);
+                },
+                highlight : function(element, errorClass, validClass){
+                    $(element).addClass('is-invalid');
+                },
+                unhighlight : function(element, errorClass, validClass){
+                    $(element).removeClass('is-invalid');
+                },
+            });
+        });
+
+    </script>
 
 @endsection
